f2e8b49e4b2baf8cf987b30a48ab6dd469b9d150
BryanMunz <116124963+BryanMunz@users.noreply.github.com>
2022-11-05 07:44:07 -0600

    Delete Helpers directory

--- Helpers/funciones_generales.php
+++
@@ -1,7 +0,0 @@
-<?php
-function path_img($folder = '', $img = '')
-{
-
-    $path = ($folder != "") ? '../img' . $img : './portal/img' . $img;
-    return (file_exists($path) ? $path : ($folder != "" ? '../img/no-image-.png' : './portal/img/no-image-.png'));
-}
